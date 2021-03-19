688b2657117a4d5f2254d184aaeca357e2af3de8
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-19 15:39:20 +0700

    1

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'bal',
     description: "check balance",
-    async execute(client, message, args, Discord, profileData) {
+    execute(client, message, args, Discord, profileData) {
         message.channel.send(`${message.author.username}, your balance is ${profileData.dollar}.`);
     }
 }
