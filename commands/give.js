1923d7d14d4d02034f8985dfe01ee377f1b33c0d
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-20 16:28:33 +0700

    1

--- commands/give.js
+++ commands/give.js
@@ -14,7 +14,7 @@ module.exports = {
                 message.channel.send("Press Y to confirm, N to cancel").then(() => {
                 message.channel.awaitMessages(filter, {
                     max: 1,
-                    time: 1000,
+                    time: 10000,
                     errors: ['time']
                  })
                 .then(message => {
