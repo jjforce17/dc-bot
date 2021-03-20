eb8ffa8be6b0b485a3bd35c9049d6b4b34f7d0bf
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-20 16:14:09 +0700

    2

--- commands/give.js
+++ commands/give.js
@@ -9,33 +9,49 @@ module.exports = {
         const target = message.mentions.users.first();
         if(amount % 1 != 0) return message.channel.send("Value must be a whole number");
         if(!target) return;
-        const user = message.author.id;
         try {
+            let filter = m => m.author.id === message.author.id
+                message.channel.send("Press Y to confirm, N to cancel").then(() => {
+                message.channel.awaitMessages(filter, {
+                    max: 1,
+                    time: 10,
+                    errors: ['time']
+                 })
+                .then(message => {
+                    message = message.first()
+                    if (message.content.toUpperCase() == 'Y') {
+                    
+                    } else if (message.content.toUpperCase() == 'N') {
+                    message.channel.send('Cancelled');
+                    } else {
+                    message.channel.send('Invalid response');
+                    }
+                    })
+                   .catch(collected => {
+                        message.channel.send('Timed out');
+                    });
+            })
             const targetData = await profileModel.findOne({userID: target.id});
             if (!targetData) return;
-            if (message.author.id = user) {
-                if (message.content == y){
-                await profileModel.findOneAndUpdate({
-                    userID: message.author.id,
-                }, 
-                {
-                    $inc : {
-                        dollar: -amount,
-                    },
-                }
-            );
-                await profileModel.findOneAndUpdate({
-                    userID: target.id,
-                }, 
-                {
-                    $inc : {
-                        dollar: amount,
-                    },
-                }
-            );
-            message.channel.send("given" + target.username + amount);
-        }
+            await profileModel.findOneAndUpdate({
+                userID: message.author.id,
+            }, 
+            {
+                $inc : {
+                    dollar: -amount,
+                },
             }
+        );
+            message.channel.send("given" + target.username + amount);
+            await profileModel.findOneAndUpdate({
+                userID: target.id,
+            }, 
+            {
+                $inc : {
+                    dollar: amount,
+                },
+            }
+        );
         } catch (err) {
             console.log(err);
             }
