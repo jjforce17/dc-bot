fcb4d5c560b8d107311ad5da8123ae65d2299a9e
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 11:00:23 +0700

    1

--- commands/test3.js
+++ commands/test3.js
@@ -3,11 +3,6 @@ module.exports = {
     description: "this is the ping command!",
     execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        if (!args.length) return;
-        const amount  = args[1];
-        const target = message.mentions.users.first();
-        if(amount % 1 != 0) return message.channel.send("Value must be a whole number");
-        if(!target) return;
         try {
             let filter = m => m.author.id === message.author.id
                 message.channel.send("Press Y to confirm, N to cancel").then(() => {
@@ -16,24 +11,8 @@ module.exports = {
                     time: 20000,
                     errors: ['time']
                  })
-                .then(async (message) => {
-                    if (message.content.toUpperCase() == 'Y') {
-                    message.channel.send('Y');
-                    } else if (message.content.toUpperCase() == 'N') {
-                    message.channel.send('N');
-                    } else if (message.content.toUpperCase() == 'Z') {
-                    message.channel.send('Z');
-                    } else if (message.content.toUpperCase() == 'V') {
-                    message.channel.send('V');
-                    return;
-                    } else {
-                    message.channel.send('Invalid response');
-                    return;
-                    }
-                    })
-                   .catch(collected => {
-                        message.channel.send('Timed out');
-                    });
+                 .then(collected => console.log(collected.size))
+                 .catch(collected => console.log(`After a minute, only ${collected.size} out of 4 voted.`));
             })
             
         } catch (err) {
