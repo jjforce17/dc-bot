5069e4b2560499a32c099ecf956303513edb1bea
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 19:50:28 +0700

    2

--- commands/test3.js
+++ commands/test3.js
@@ -4,20 +4,29 @@ module.exports = {
     execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
         try {
-            const filter1 = r1 => r1.author.id === message.author.id
-                message.channel.send("Press Y to confirm, N to cancel").then(() => {
+            const target = message.mentions.users.first();
+            if(!target) return;
+            const filter1 = r1 => r1.author.id === message.author.id;
                 message.channel.awaitMessages(filter1, {
                     max: 1,
                     time: 20000,
                     errors: ['time']
                  })
-                 .then((collected1) => {
-                    const r1 = collected1.first()
-                    if (r1.content.toUpperCase() == 'Y') {
+                 .then((collected) => {
+                    const filter2 = r2 => r2.content === "y" || r2.content === "Y" || r2.content === "n" || r2.content === "N"
+                message.channel.send("Press Y to confirm, N to cancel").then(() => {
+                message.channel.awaitMessages(filter2, {
+                    max: 1,
+                    time: 20000,
+                    errors: ['time']
+                 })
+                 .then((collected2) => {
+                    const r2 = collected2.first();
+                    if (r2.content.toUpperCase() == 'Y') {
                     message.channel.send('Y');
                     
                     } 
-                    else if (r1.content.toUpperCase() == 'N') {
+                    else if (r2.content.toUpperCase() == 'N') {
                     message.channel.send('N');
                 
                     } else {
@@ -28,29 +37,7 @@ module.exports = {
                     message.channel.send('Timed out');
                 });
             })
-            const filter2 = r2 => r2.author.id === message.author.id
-                message.channel.send("Press Y to confirm, N to cancel").then(() => {
-                message.channel.awaitMessages(filter2, {
-                    max: 1,
-                    time: 20000,
-                    errors: ['time']
-                 })
-                 .then((collected2) => {
-                    const r2 = collected2.first()
-                    if (r2.content.toUpperCase() == 'Z') {
-                    message.channel.send('Z');
-                    } 
-                    else if (r2.content.toUpperCase() == 'V') {
-                    message.channel.send('V'); 
-                    } else {
-                    message.channel.send('Invalid response');
-                    }
                 })
-                .catch(collected => {
-                    message.channel.send('Timed out');
-                });
-            })
-            
         } catch (err) {
             console.log(err);
             }
