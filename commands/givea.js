7c69af183602d40c426a5f293db1b12e5c09ff1c
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 09:46:35 +0700

    2

--- commands/givea.js
+++ commands/givea.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'givea',
     description: "admin give",
     async execute(client, message, args, Discord) {
-        if (message.member.roles.cache.has("803841418589110303")){
+        if(!message.member.roles.cache.some(r => r.name === "Big boi")) reutrn ;
         if (!args.length) return;
         const amount  = args[1];
         const target = message.mentions.users.first();
@@ -26,9 +26,5 @@ module.exports = {
         } catch (err) {
             console.log(err);
             }
-        }
-        else {
-            return message.channel.send("error");
-        }
     }
 }
