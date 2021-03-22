c1daa1b82d3c5372c9b2a22f35a57e3097230c57
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 11:32:58 +0700

    2

--- commands/test3.js
+++ commands/test3.js
@@ -12,7 +12,6 @@ module.exports = {
                     errors: ['time']
                  })
                  .then(async (message) => {
-                    message = message.first()
                     if (message.content.toUpperCase() == 'Y') {
                     message.channel.send('Y');
                     console.log("y");
