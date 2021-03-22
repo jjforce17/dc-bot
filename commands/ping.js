7b456c5be2e1a747d65f725c7234444eff62b1b3
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 09:06:49 +0700

    2

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,9 @@ module.exports = {
     execute(client, message, args, Discord) {
         message.channel.send('pong');
         let welcomeRole = message.member.roles.cache.find(r => r.name === "boi");
+        message.channel.send(welcomeRole);
         if(!message.member.roles.cache.find(r => r.name === "boi")){
-            message.channel.send(welcomeRole);
+            message.channel.send("test");
         }
         message.member.roles.add(welcomeRole).catch(console.error);
     }
