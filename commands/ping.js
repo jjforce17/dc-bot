fb78ad4028f972d8e37e151d721f3fc2b116db5e
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-22 08:43:15 +0700

    1

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@ module.exports = {
     description: "this is the ping command!",
     execute(client, message, args, Discord) {
         message.channel.send('pong');
-            let welcomeRole = message.guild.roles.cache.find(role => role.name === 'boi');
-            member.roles.add(welcomeRole);
+        let welcomeRole = message.guild.roles.cache.find(role => role.name === 'boi');
+        let member = message.member;
+        member.roles.add(welcomeRole);
     }
 }
