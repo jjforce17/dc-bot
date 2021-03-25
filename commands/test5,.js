5aad4ebaa6e2a4a3e583072f83dfc6beebac7415
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-25 10:59:58 +0700

    1

--- commands/test5,.js
+++ commands/test5,.js
@@ -7,22 +7,19 @@ module.exports = {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
         const botID = "803868333341802499";
         try {
-            await profileModel.findOneAndUpdate({
-                userID: botID,
-            }, 
-            {
-                $set: {
+            await profileModel.findOneAndUpdate({ userID: botID }, 
+            {$set: {
                     BCard1: 0,
                     BCard2: 0,
                     BCard3: 0,
                     BCard4: 0,
                     BCard5: 0,
-                    player1: "",
-                    player2: "",
-                    player3: "",
+                    player1: "000",
+                    player2: "000",
+                    player3: "000",
                 }
             }
-        );
+        )
         } catch (err) {
             console.log(err);
             }
