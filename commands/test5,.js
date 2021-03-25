ef64d37036d08a59a36002904708aecb35682105
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-25 10:49:16 +0700

    2

--- commands/test5,.js
+++ commands/test5,.js
@@ -12,14 +12,14 @@ module.exports = {
             }, 
             {
                 $set: {
-                    BCard1 : 0,
-                    BCard2 : 0,
-                    BCard3 : 0,
-                    BCard4 : 0,
-                    BCard5 : 0,
-                    player1 : "",
-                    player2 : "",
-                    player3 : "",
+                    BCard1: 0,
+                    BCard2: 0,
+                    BCard3: 0,
+                    BCard4: 0,
+                    BCard5: 0,
+                    player1: "",
+                    player2: "",
+                    player3: "",
                 }
             }
         );
