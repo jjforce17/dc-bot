2c3debcff5dad3a4e150351003563e809790bda5
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-25 11:17:51 +0700

    2

--- commands/test6.js
+++ commands/test6.js
@@ -11,7 +11,7 @@ module.exports = {
         }, 
         {
             $set : {
-                BCard1: 0,
+                dollar: 0,
             },
         }
     );
--- models/profileSchema.js
+++ models/profileSchema.js
@@ -4,7 +4,15 @@ const profileSchema = new mongoose.Schema({
     userID: { type: String, require: true, unique: true },
     serverID: { type: String, require: true },
     dollar: { type: Number, default: 100 },
-    bank: { type: Number }
+    bank: { type: Number },
+    BCard1: { type: Number },
+    BCard2: { type: Number },
+    BCard3: { type: Number },
+    BCard4: { type: Number },
+    BCard5: { type: Number },
+    player1: { type: String },
+    player2: { type: String },
+    player3: { type: String }
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
