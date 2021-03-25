6e4d1269511838a264041dc88f6a47cf8fee30ae
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-25 10:08:40 +0700

    1

--- commands/test6.js
+++ commands/test6.js
@@ -1,8 +1,8 @@
 const profileModel = require('../models/profileSchema');
 
 module.exports = {
-    name: 'testg5',
-    description: "testg5",
+    name: 'testg6',
+    description: "testg6",
     async execute(client, message, args, Discord, profileData) {
         const user = args[0];
         const fuser = message.guild.members.get(user.id);
