33e5796565c7236331f0eb5cb75b1c02ebe6dd6d
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-25 11:07:01 +0700

    3

--- commands/psst2.js
+++ commands/psst2.js
@@ -1,8 +1,8 @@
 const profileModel = require('../models/profileSchema');
 
 module.exports = {
-    name: 'psst2',
-    description: "psst2",
+    name: 'psst333',
+    description: "psst333",
     async execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
         const response = await profileModel.findOneAndUpdate({
--- commands/test6.js
+++ commands/test6.js
@@ -2,10 +2,18 @@ const profileModel = require('../models/profileSchema');
 
 module.exports = {
     name: 'testg6',
-    description: "testg6",
+    description: "test6",
     async execute(client, message, args, Discord, profileData) {
-        const user = args[0];
-        client.users.cache.find(u => u.id === user);
+        if(!message.member.roles.cache.some(r => r.name === "boi")) return;
+        const botID = "803868333341802499";
+        const response = await profileModel.findOneAndUpdate({
+            userID: botID,
+        }, 
+        {
+            $set : {
+                Bcard1: 0,
+            },
+        }
+    );
     }
 }
-
