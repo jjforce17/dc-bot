84fefa440ca1aa1ee76f0a888ff92e53659030c6
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-26 21:13:03 +0700

    1

--- commands/test6.js
+++ commands/test6.js
@@ -3,17 +3,31 @@ const profileModel = require('../models/profileSchema');
 module.exports = {
     name: 'testg6',
     description: "test6",
-    async execute(client, message, args, Discord, profileData) {
+    async execute(client, message, args, Discord) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        const botID = "803868333341802499";
-        const response = await profileModel.findOneAndUpdate({
-            userID: botID,
-        }, 
-        {
-            $set : {
-                dollar: 0,
-            },
-        }
-    );
+        try {
+            let filter = m => m.author.id === message.author.id && message.content == "y" || message.content == "Y";
+                message.channel.send("Press Y to confirm, N to cancel").then(() => {
+                message.channel.awaitMessages(filter, {
+                    max: 1,
+                    time: 10000,
+                    errors: ['time']
+                 })
+                .then(async (message) => {
+                    message = message.first()
+                    if (message.content.toUpperCase() == 'Y') {
+                            message.channel.send("test");
+                    } else {
+                    message.channel.send('Invalid response');
+                    }
+                    })
+                   .catch(collected => {
+                        message.channel.send('Timed out');
+                    });
+            })
+            
+        } catch (err) {
+            console.log(err);
+            }
     }
 }
