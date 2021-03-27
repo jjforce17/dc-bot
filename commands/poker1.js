f2ba855626cd4d0b6513f64c4c3019e97c8e0e53
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-03-27 19:54:34 +0700

    2

--- commands/poker1.js
+++ commands/poker1.js
@@ -5,9 +5,7 @@ module.exports = {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
         if(message.member.roles.cache.some(r => r.name === "Poker1")) return;
         var rolea = message.guild.roles.cache.find(r => r.name === "Poker1");
-        console.log(rolea.id);
         var mRole = message.guild.roles.cache.get(rolea.id).members;
-        console.log(mRole);
         if(mRole.size == 3) return console.log("3");
         message.member.roles.add(rolea).catch(console.error);
     }
