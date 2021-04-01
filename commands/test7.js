1f4607bb4dd74cc71c5e80b3575c59717bb30f8b
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-04-01 10:52:44 +0700

    4

--- commands/test7.js
+++ commands/test7.js
@@ -8,21 +8,14 @@ module.exports = {
         var card1 = Math.floor(Math.random() * 3) + 1;
         var card2 = Math.floor(Math.random() * 3) + 1;
         var card3 = Math.floor(Math.random() * 3) + 1;
-        while (card2 == card1) {
+        while (card2 == card1 || card2 == card3) {
             card2 = Math.floor(Math.random() * 3) + 1;
             console.log(card2);
         }
-        console.log("card 2 != card 1")
-        while (card3 == card1) {
+        while (card3 == card1 || card3 == card2) {
             card3 = Math.floor(Math.random() * 3) + 1;
             console.log(card3);
         }
-        console.log("card 3 != card 1")
-        while (card3 == card2) {
-            card2 = Math.floor(Math.random() * 3) + 1;
-            console.log(card3);
-        }
-        console.log("card 3 != card 2")
         message.channel.send(card1);
         message.channel.send(card2);
         message.channel.send(card3);
