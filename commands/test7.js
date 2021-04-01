f4d669134e52a261aaa665c2fa25cbc6a6ce87ff
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-04-01 10:49:38 +0700

    2

--- commands/test7.js
+++ commands/test7.js
@@ -5,19 +5,19 @@ module.exports = {
     description: "test7",
     execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        var card1 = Math.floor(Math.random() * 4) + 1;
-        var card2 = Math.floor(Math.random() * 4) + 1;
-        var card3 = Math.floor(Math.random() * 4) + 1;
+        var card1 = Math.floor(Math.random() * 3) + 1;
+        var card2 = Math.floor(Math.random() * 3) + 1;
+        var card3 = Math.floor(Math.random() * 3) + 1;
         while (card2 == card1) {
-            card2 = Math.floor(Math.random() * 4) + 1;
+            card2 = Math.floor(Math.random() * 3) + 1;
             console.log(card2);
         }
         while (card3 == card1) {
-            card3 = Math.floor(Math.random() * 4) + 1;
+            card3 = Math.floor(Math.random() * 3) + 1;
             console.log(card3);
         }
         while (card3 == card2) {
-            card2 = Math.floor(Math.random() * 4) + 1;
+            card2 = Math.floor(Math.random() * 3) + 1;
             console.log(card3);
         }
         message.channel.send(card1);
