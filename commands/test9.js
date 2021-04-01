4a79ef6c613831e2ac1337ca4254a322eb253309
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-04-01 09:52:02 +0700

    3

---
+++ commands/test9.js
@@ -0,0 +1,38 @@
+const profileModel = require('../models/profileSchema');
+
+module.exports = {
+    name: 'testg9',
+    description: "test9",
+    async execute(client, message, args, Discord, profileData) {
+        var PlayerRandNum = Math.floor(Math.random() * 3) + 1;
+        var place = 0;
+        var user1 = 123;
+        var user2 = 0;
+        var user3 = 123;
+        if (botData.player1 == message.author.id) return message.channel.send("You already registered.");
+        if (PlayerRandNum == 1) {
+            if (user1 != 0) {
+                while (PlayerRandNum == 1) {
+                    PlayerRandNum = Math.floor(Math.random() * 3) + 1;
+                }
+            }
+        }
+        else if (PlayerRandNum == 2) {
+            if (user2 != 0) {
+                while (PlayerRandNum == 2) {
+                    PlayerRandNum = Math.floor(Math.random() * 3) + 1;
+                }
+            }
+            
+        }
+        else if (PlayerRandNum == 3) {
+            if (user3 != 0) {
+                while (PlayerRandNum == 3) {
+                    PlayerRandNum = Math.floor(Math.random() * 3) + 1;
+                }
+            }
+        }
+        place = PlayerRandNum;
+    message.channel.send(place);
+    }
+}
