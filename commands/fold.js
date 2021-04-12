315d9d09a3797cbefe7d98e9543ab07f7fd9a4a3
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-04-12 09:13:54 +0700

    2

--- commands/fold.js
+++ commands/fold.js
@@ -7,6 +7,9 @@ module.exports = {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
         if(!message.member.roles.cache.some(r => r.name === "Poker1")) return message.channel.send("Please join a room");
         const botID = "803868333341802499";
+        const Player1ID = botData.player1;
+        const Player2ID = botData.player2;
+        const Player3ID = botData.player3;
         botData = await profileModel.findOne({ userID: botID });
         if (message.author.id == Player1ID) {
             if (botData.Player1Turn == false) {
