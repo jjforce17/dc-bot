f6c020f77e5ef0d0d9b4dbbe244406d108b43614
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-04-18 21:42:46 +0700

    3

--- commands/test10.js
+++ commands/test10.js
@@ -5,6 +5,7 @@ module.exports = {
     description: "test10",
     async execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "Big boi")) return;
+        const botID = "803868333341802499";
         const botData = await profileModel.findOne({ userID: botID });
         if (botData.player1 == "000") return message.channel.send("User not found");
         const user = Client.fetchUser(botData.player1);
