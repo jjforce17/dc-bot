45eb5db53ec338bcaba55e695170d4f86f04105d
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-06-13 20:44:47 +0700

    4

--- commands/psst.js
+++ commands/psst.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "psst",
     async execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        if(!message.channel.name == "psst") return console.log("not rc");
+        if(!message.channel.cache.find(c => c.name === "psst")) return console.log("rc");
         const response = await profileModel.findOneAndUpdate({
             userID: message.author.id,
         }, 
@@ -13,7 +13,7 @@ module.exports = {
             $inc : {
                 dollar: 1,
             },
-
+            
         }
     );
     }
