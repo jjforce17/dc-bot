e3668572e8df81b43b1400c3858b55fc5d6aabf9
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-06-13 20:48:26 +0700

    2

--- commands/psst.js
+++ commands/psst.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "psst",
     async execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        if(!message.channel.cache.find(c => c.name === "psst")) return console.log("rc");
+        if(!message.channel.name != "psst") return console.log("rc");
         const response = await profileModel.findOneAndUpdate({
             userID: message.author.id,
         }, 
