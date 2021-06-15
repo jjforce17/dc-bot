60ef4e2f5af1a5d44fb04e19c9da7c9e4bb89a07
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2021-06-15 17:44:16 +0700

    3

--- commands/digitsc.js
+++ commands/digitsc.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "this is the ping command!",
     async execute(client, message, args, Discord, profileData) {
         if(!message.member.roles.cache.some(r => r.name === "boi")) return;
-        if(!message.member.roles.cache.some(r => r.name === "horni boi")) return message.channel.send("You are already have this.");
+        if(message.member.roles.cache.some(r => r.name === "horni boi")) return message.channel.send("You are already have this.");
         if(message.channel.name != "shop") return;
         var rolea = message.guild.roles.cache.find(r => r.name === "horni boi");
         const mooney = profileData.dollar 
